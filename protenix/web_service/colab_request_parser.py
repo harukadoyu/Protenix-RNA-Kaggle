9574345c32953038def94190e95bf32362316510
hd702 <haru.doyu@gmail.com>
2025-05-21 10:37:19 +0300

    fix paths

--- protenix/web_service/colab_request_parser.py
+++ protenix/web_service/colab_request_parser.py
@@ -37,8 +37,8 @@ MMSEQS_SERVICE_HOST_URL = os.getenv(
 )
 MAX_ATOM_NUM = 60000
 MAX_TOKEN_NUM = 5000
-DATA_CACHE_DIR = "/af3-dev/release_data/"
-CHECKPOINT_DIR = "/af3-dev/release_model/"
+DATA_CACHE_DIR = "af3-dev/release_data/"
+CHECKPOINT_DIR = "af3-dev/release_model/"
 
 
 def download_tos_url(tos_url, local_file_path):
